c192d7dbef8542fd160360b0be28af554421e84f
Spec Ussa <serega.ussa@gmail.com>
2023-09-14 11:33:35 +0300

    add filter sort

--- js/main.js
+++ js/main.js
@@ -341,9 +341,6 @@ if(!catalogp){} else {
 
 // start range slider
 const rangeslider = document.getElementById('filterPrice');
-const rangeMin = parseInt(rangeslider.dataset.min);
-const rangeMax = parseInt(rangeslider.dataset.max);
-const rangestep = parseInt(rangeslider.dataset.step);
 const filterInputs = document.querySelectorAll('.filter__input');
 var frb = document.querySelector('.filter__range_button');
 var minValue = document.querySelector('.filter__range_min');
@@ -362,6 +359,9 @@ var uncheck = document.getElementsByClassName('filter__check_hidden');
 var fbutton = document.querySelector('.filter__button');
 
 if(!rangeslider){} else {
+  const rangeMin = parseInt(rangeslider.dataset.min);
+  const rangeMax = parseInt(rangeslider.dataset.max);
+  const rangestep = parseInt(rangeslider.dataset.step);
   noUiSlider.create(rangeslider, {
       start: [rangeMin, rangeMax],
       connect: true,
@@ -510,6 +510,72 @@ if(!catalofl){} else {
   })
 }
 // end filter buttons
+
+// start select
+const SELECT = '[data-select]'
+const SELECT_LIST = '[data-select-list]'
+const SELECT_ARROW = '[data-select-arrow]'
+const SELECT_ACTION = '[data-select-action]'
+const SELECT_TITLE = '[data-select-title]'
+const SELECT_INPUT = '[data-select-input]'
+const SELECT_ITEM = 'selectItem'
+const OPEN_SELECT = 'selectOpen'
+
+class Select {
+  static attach() {document.querySelectorAll(SELECT).forEach(select => new Select().init(select))}
+  init(select) {if (this.findSelect(select)) {this.applyListener()}}
+  applyListener() {
+    document.querySelector('*').addEventListener('click', e => {
+      const element = this.select.contains(e.target) && e.target.closest(SELECT_ACTION)
+      if (this.isCallSelectElement(element)) {
+        if (this.isOpened()) {
+          this.closeSelectList();
+        } else {
+          this.openSelectList()
+        }
+      }
+      if (this.isCallSelectItemElement(element)) {
+        this.addSelectedValue(element)
+      }
+      if (this.isCallSelectElement(element) !== true && this.selectOverlayIsClickedElement(element) !== true) {
+        this.closeSelectList()
+      }
+    })
+  }
+  isCallSelectElement(element, target) {return element && OPEN_SELECT in element.dataset}
+  isCallSelectItemElement(element, target) {return element && SELECT_ITEM in element.dataset}
+  findSelect(select) {
+    if (select) {
+      this.select = select
+      this.selectList = this.select.querySelector(SELECT_LIST)
+      this.selectArrow = this.select.querySelector(SELECT_ARROW)
+      this.selectTitle = this.select.querySelector(SELECT_TITLE)
+      this.selectInput = this.select.querySelector(SELECT_INPUT)
+      return true
+    }
+    return false
+  }
+  isOpened() {return this.selectList.classList.contains('filter__select_list_opened')}
+  openSelectList() {
+    this.selectList.style.maxHeight = this.selectList.scrollHeight + "px";
+    this.selectList.classList.add('filter__select_list_opened');
+    this.selectArrow.classList.add('filter__select_arrow_rotate');
+  }
+  closeSelectList() {
+    this.selectList.style.maxHeight = null;
+    this.selectList.classList.remove('filter__select_list_opened');
+    this.selectArrow.classList.remove('filter__select_arrow_rotate');
+  }
+  addSelectedValue(element) {
+    this.selectTitle.innerHTML = element.innerHTML;
+    this.selectInput.value = element.innerHTML;
+    this.selectInput.setAttribute('value', this.selectInput.value);
+  }
+  selectOverlayIsClickedElement(element, target) {return element && 'select' in element.dataset}
+}
+
+Select.attach()
+// end select
 
 // animation page
 let breadcrumb = document.querySelector('.breadcrumbs__item');
